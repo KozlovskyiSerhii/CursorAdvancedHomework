43c8b2c928564c1195f693e6739feff15b89b2b6
Serhii <kozlovskijserhij@gmail.com>
2023-03-04 01:38:37 +0200

    add work function

--- homework.2/script.js
+++ homework.2/script.js
@@ -1,4 +1,30 @@
-let numberN = prompt('введіть число N');
-let numberM = prompt('введіть число M');
+let numberN = parseInt(prompt('введіть число N'));
+let numberM = parseInt(prompt('введіть число M'));
+let sayTrue = prompt('true or false');
+console.log(numberN);
+console.log(numberM);
+console.log(sayTrue);
 
-console.log(+numberN +numberM);
+let sumNM = 0;
+for (let i = numberN; i <= numberM; i++){1
+   if (sayTrue === 'true') {
+      if (i % 2 === 0) {
+      continue;
+   }
+sumNM += i;
+   } else if (sayTrue === 'false') {
+      sumNM += i;
+}
+   }
+console.log(sumNM);
+
+
+// if ((isNaN(numberN) || isNaN(numberM)) === false) {
+//    if ((numberN % 1 === 0 && numberM % 1 === 0)) {
+//    console.log('ціле число ');
+// } else {
+//    console.log('неціле число');
+// }
+// } else {
+//    console.log('Памятайте, ЧИСЛО, не РЯДОК!');
+// }
