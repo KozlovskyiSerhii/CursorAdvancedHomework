224fb2a80b2aff3203d49091b404c1e33999032e
Serhii <kozlovskijserhij@gmail.com>
2023-03-04 02:09:46 +0200

    done

--- homework.2/script.js
+++ homework.2/script.js
@@ -1,13 +1,43 @@
 let numberN = parseInt(prompt('введіть число N'));
 let numberM = parseInt(prompt('введіть число M'));
 let trueOrFalse = confirm("true or false");
-let sayTrue = prompt('true or false');
-// let sayTrue = prompt('true or false');
 console.log(numberN);
 console.log(numberM);
 console.log(trueOrFalse);
-console.log(sayTrue);
 
+
+//якщо ввести не цифри, а інші
+if ((isNaN(numberN) || isNaN(numberM)) === false) {
+   if ((numberN % 1 === 0 && numberM % 1 === 0)) {
+   console.log('ціле число ');
+} else {
+   console.log('неціле число');
+}
+} else {
+   alert('Памятайте, ЧИСЛО, не РЯДОК!');
+}
+
+let sumNM = 0;
+
+for (let i = numberN; i <= numberM; i++){
+   if (trueOrFalse) {
+      if (i % 2 === 0) {
+      continue;
+   }
+sumNM += i;
+   } else {
+      sumNM += i;
+}
+   }
+console.log(sumNM);
+document.writeln(`<h2>${sumNM}</h2>`);
+// те саме тільки потрібно ввести тру або фолс
+/*let numberN = parseInt(prompt('введіть число N'));
+let numberM = parseInt(prompt('введіть число M'));
+let sayTrue = prompt('true or false');
+console.log(numberN);
+console.log(numberM);
+console.log(sayTrue);
 
 
 //якщо ввести не цифри, а інші
@@ -23,17 +53,6 @@ if ((isNaN(numberN) || isNaN(numberM)) === false) {
 
 let sumNM = 0;
 for (let i = numberN; i <= numberM; i++){
-   if (trueOrFalse) {
-      if (i % 2 === 0) {
-      continue;
-   }
-sumNM += i;
-   } else {
-      sumNM += i;
-}
-   }
-console.log(sumNM);
-for (let i = numberN; i <= numberM; i++){
    if (sayTrue === 'true') {
       if (i % 2 === 0) {
       continue;
@@ -43,5 +62,4 @@ sumNM += i;
       sumNM += i;
 }
    }
-console.log(sumNM);
-
+console.log(sumNM);*/
