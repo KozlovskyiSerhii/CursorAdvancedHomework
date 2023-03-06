be0b4e6cae35862ccfc9e664a2bcf527e600cb7d
Serhii <kozlovskijserhij@gmail.com>
2023-03-07 00:03:18 +0200

    added actions for non-integers

--- homework.2/script.js
+++ homework.2/script.js
@@ -3,14 +3,12 @@ let numberM = prompt('введіть число M');
 let trueOrFalse = confirm("true or false");
 console.log(numberN);
 console.log(numberM);
-
+let sumNM = 0;
 
 //якщо ввести не цифри, а інші
-if ((isNaN(numberN) || isNaN(numberM)) === false) {
-   if ((numberN % 1 === 0 && numberM % 1 === 0)) {
+if (!isNaN(numberN) && !isNaN(numberM)) {
+   if ((+numberN % 1 === 0 && +numberM % 1 === 0)) {
       console.log('Введено ціле число');
-   let sumNM = 0;
-
    for (let i = +numberN; i <= +numberM; i++) {
       if (trueOrFalse) {
       if (i % 2 === 0) {
@@ -22,8 +20,18 @@ if ((isNaN(numberN) || isNaN(numberM)) === false) {
    console.log(sumNM);
    document.writeln(`<h2>${sumNM}</h2>`);
    } else {
+      for (let i = +Math.floor(+numberN); i <= +Math.floor(+numberM); i++) {
+      if (trueOrFalse) {
+      if (i % 2 === 0) {
+         continue;
+      }
+      } 
+      sumNM += i;
+      }
    console.log('неціле число');
    document.writeln(`<h2>Введіть ціле число</h2>`);
+   console.log(sumNM);
+   document.writeln(`<h2>${sumNM}</h2>`);
    }
 } else {
    alert('Памятай, ЧИСЛО, не РЯДОК!');
