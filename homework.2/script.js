193a701a438ab498d8f1a35d9b4643c08d7b0589
Serhii <kozlovskijserhij@gmail.com>
2023-03-11 01:52:18 +0200

    some big fix with while

--- homework.2/script.js
+++ homework.2/script.js
@@ -1,43 +1,29 @@
-let numberN = prompt('введіть число N');
-let numberM = prompt('введіть число M');
-let trueOrFalse = confirm("true or false");
-console.log(numberN);
-console.log(numberM);
+let numberN= +prompt('Введіть перше ціле число');
+let numberM = +prompt('Введіть друге ціле число');
 let sumNM = 0;
 
-//якщо ввести не цифри, а інші
-if (!isNaN(numberN) && !isNaN(numberM)) {
-   if ((+numberN % 1 === 0 && +numberM % 1 === 0)) {
-      console.log('Введено ціле число');
-   for (let i = +numberN; i <= +numberM; i++) {
-      if (trueOrFalse) {
-      if (i % 2 === 0) {
-         continue;
-      }
-      } 
-      sumNM += i;
-   }
-   console.log(sumNM);
-   document.writeln(`<h2>${sumNM}</h2>`);
-   } else {
-      console.log('введено неціле число');
-      for (let i = +Math.floor(+numberN); i <= +Math.floor(+numberM); i++) {
-      if (trueOrFalse) {
-      if (i % 2 === 0) {
-         continue;
-      }
-      } 
-      sumNM += i;
-      }
-  
-   document.writeln(`<h2>Введіть ціле число</h2>`);
-   console.log(sumNM);
-   document.writeln(`<h2>${sumNM}</h2>`);
-   }
-} else {
-   alert('Памятай, ЧИСЛО, не РЯДОК!');
-}
+while ( !Number.isInteger(numberN) || numberN <= 0) {
+   numberN = parseInt(prompt('enter first number'));
+};
+while (!Number.isInteger(numberM) || numberM <= 0) {
+   numberM = parseInt(prompt('enter  second number'))
+};
 
-let b = parseInt('11,7088787')
-console.log(b);
+if (numberN > numberM) {
+   let x = numberN;
+   numberN = numberM;
+   numberM = x;
+}
+let trueOrFalse = confirm("пропускаємо парні чи ні?");
+   
+   for (let i = parseInt(numberN); i <= parseInt(numberM); i++) {
+      if (!trueOrFalse) {   
+            sumNM += i;     
+      } else if (i % 2 !== 0) {
+         sumNM += i;
+      }
+   }
+   document.writeln(`<h2>${sumNM}</h2>`);
+
+
 
