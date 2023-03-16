693b4e584849bb2abe4903a1655ae374a54d55ad
Serhii <kozlovskijserhij@gmail.com>
2023-03-16 15:39:53 +0200

    add new function

--- homework.arrayFunction/script.js
+++ homework.arrayFunction/script.js
@@ -1,7 +1,10 @@
 const getDividedByFives = [6, 2, 55, 11.1, 78, 2, 55.1, 77, 57, 87, 23, 2, 56, 25 , 45, 5, 85, 95, 5, 35, 25, 20, 40, 50, 100, 3, 2, 43, 34, 36, 86, 89, 23, 12, 15, 10, 1, 44, 46
 ];
 const number = [0.1, 1, 2, 4, 17, 11, 9, 2.2, 1.4, 98, 76, 20, 24.5, 13.4, 3, 11, 6, 8, 16, 191];
-	//функція з фільтром на діляться що діляться на 5 через 'for'
+	
+
+
+//функція виводить числа кратні 5
 function getDividedByFive(array){
 	const getDivided = [];
 	for (let i = 0; i < array.length; i++) {
@@ -11,7 +14,9 @@ function getDividedByFive(array){
 	}
 	return getDivided;
 }
-//функція з фільтром на діляться що діляться на 5
+
+
+//функція виводить числа кратні 5
 function getDividedByFiveFilter(array) {
 	const int = array.filter((item, index, arr) => item % 5 === 0);
 	return int;
@@ -30,6 +35,9 @@ function result(array) {
 }
 console.log(result(number));
 
+
+
+//рахує медіану
 function getMedian(array) {
 	const int = array.filter((item, index) => Number.isInteger(item));
 	const intSort = int.sort((a, b) => a - b);
@@ -45,6 +53,7 @@ function getMedian(array) {
 	}
 }
 console.log(getMedian(number));
+
 
 // виводить масив випадкових чисел із заданою кількістю елементів, та гранрицями мін і макс значення
 function getRandomArray(length, min, max){
@@ -62,38 +71,52 @@ console.log(getRandomArray(40, 2000, 5));
 
 
 
+//рахує кількість чисел більших від нуля в масиві
+const numberMin = [-1, -4, 5, 6, 7, 0, -8, 9, -2];
+	function countPositiveNumbers(array){
+	const int = array.filter((item) => item > 0);
+	return int.length;
+};
+console.log(countPositiveNumbers(numberMin));
 
 
 
+function generateCombinations(string) {
+   if (string <= 10) {
+      if (string.length === 1) {
+         return [string];
+      }
+      const result = [];
+      for (let i = 0; i < string.length; i++) {
+         const letter = string[i];
+         //console.log('letter', letter)
+         const cutLetter = string.slice(0, i) + string.slice(i + 1);
+         const combinations = generateCombinations(cutLetter);
+         //console.log('combination', combinations)
+         combinations.map((combination) => {
+            result.push(letter + combination);
+         });
+      }
+      return result;
+   }  else {
+   return alert('слово занадто довге для комбінування ')
+}
+}
+console.log(generateCombinations('aaadвввіввлфілдфіьсдьіфдсьфідьсфіьсдфіьсщфі'));
 
-const arr = ['spider-man', 'Iron-man', 'Capitan America'];
-const numbers = [1, 12, 14, 15, 51, 57, 77];
+//заміна емоційної лексики на зірочки
+let x = 'fucking ngbictfuck sucking fucking hstufuckyourself vpidisd idiots russian pidarashn '
 
-// arr.forEach(function (item, index, arr){
-//    console.log(item)
+function changeWord(string) {
+  const badWord = ["fuck", "shit", "suck", "idiot", "russian", "pidar"];
+  let stringWithoutBadWords = string;
+  for (let i = 0; i < badWord.length; i++) {
+     const splitString = stringWithoutBadWords.split(badWord[i]);
+   //   console.log('split string', splitString);
+    stringWithoutBadWords = splitString.join('*'.repeat(badWord[i].length));
+  }
+  return stringWithoutBadWords;
+}
 
-// });
-// const avan = [];
-// arr.forEach((item, index, arr) => {
-//    avan.push(item + ' _+_++ ' + 0);
-// 	console.log(item);
-// 	console.log(avan);
-// });
+console.log(changeWord(x))
 
-//
-// arr.map((array) => {
-//    console.log(array)
-// })
-// const res = arr.map((array, i) => {
-// 	const str = `${i + 1}, ${array}`;
-// 	return str;
-// })
-//фільтрує за наявністю рисочки в елемнті масиву. якщо є риска повертає тру, якщо ні фолс. при тру записує в результуючий масив.
-// const les = arr.filter((array) => {
-// 	return array.includes('-')
-// });
-// console.log(les)
-
-// const getDividedByFives = getDividedByFive.filter((array) => {
-// 	return array.includees()
-// })
