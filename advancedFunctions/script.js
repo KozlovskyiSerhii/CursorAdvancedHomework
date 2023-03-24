3526fd108b2dab55d8bd8165e16d05056eef02ec
Serhii <kozlovskijserhij@gmail.com>
2023-03-25 01:05:40 +0200

    change console log

--- advancedFunctions/script.js
+++ advancedFunctions/script.js
@@ -3,28 +3,27 @@ const latvia = { tax: 0.25, middleSalary: 1586, vacancies: 3921 };
 const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
 
 function getMyTaxes(salary) {
-   return (this.tax * salary).toFixed(2);
+   console.log((this.tax * salary).toFixed(2));
 }
-console.log(getMyTaxes.call(ukraine, 3000));
+getMyTaxes.call(ukraine, 3000);
 
 function getMiddleTaxes() {
-   return (this.tax * this.middleSalary).toFixed(2);
+console.log((this.tax * this.middleSalary).toFixed(2));
 }
-console.log(getMiddleTaxes.call(ukraine));
+getMiddleTaxes.call(ukraine);
 
 
 
 function getTotalTaxes() {
-   return (this.tax * this.middleSalary * this.vacancies).toFixed(2);
+console.log((this.tax * this.middleSalary * this.vacancies).toFixed(2));
 }
-console.log(getTotalTaxes.call(ukraine));
+getTotalTaxes.call(ukraine);
 
 function getMySalary() {
    const salary = Math.round(Math.random() * (2000 - 1500) + 1500);
    const taxes = +(this.tax * salary).toFixed(2);
    const profit = salary - taxes
-   return { salary, taxes, profit };
+   console.log({salary, taxes, profit });
 }
-setInterval(() => {
-   console.log(getMySalary.call(ukraine));
-}, 10000);
+getMySalary.call(ukraine);
+setInterval(() => {getMySalary.call(ukraine)}, 10000);
