c0be21de1277b00e7ca5f585ca8f20727c67a754
Serhii <kozlovskijserhij@gmail.com>
2023-04-08 19:31:20 +0300

    add new home work

---
+++ ASYNC-HW/script.js
@@ -0,0 +1,18 @@
+async function getRandomChinese(length) {
+  const chars = [];
+  let i = 0;
+  while (i < length) {
+    const sign = Date.now().toString().slice(-5);
+    chars.push(String.fromCharCode(sign));
+    await new Promise(resolve => setTimeout(resolve, 50));
+    i++;
+  }
+  return chars.join('');
+}
+async function tryFunc() {
+   console.log(await getRandomChinese(4));
+   console.log(await getRandomChinese(5));
+   console.log(await getRandomChinese(8));
+}
+
+tryFunc();
