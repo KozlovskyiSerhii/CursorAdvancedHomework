5b957ed916b3e45f203e735eb0101a77f7ea3264
Serhii <kozlovskijserhij@gmail.com>
2023-04-20 19:57:06 +0300

    new homework

--- HW-EVENT/script.js
+++ HW-EVENT/script.js
@@ -1,50 +1,56 @@
-const  tone = {
-  'KeyA': 'audio/A.wav',
-  'KeyS': 'audio/S.wav',
-  'KeyD': 'audio/D.wav',
-  'KeyF': 'audio/F.wav',
-  'KeyG': 'audio/G.wav',
-  'KeyH': 'audio/H.wav',
-  'KeyJ': 'audio/J.wav',
-  'KeyK': 'audio/K.wav',
-  'KeyL': 'audio/L.wav',
-};
+// const tone = {
+//   'KeyA': 'audio/A.wav',
+//   'KeyS': 'audio/S.wav',
+//   'KeyD': 'audio/D.wav',
+//   'KeyF': 'audio/F.wav',
+//   'KeyG': 'audio/G.wav',
+//   'KeyH': 'audio/H.wav',
+//   'KeyJ': 'audio/J.wav',
+//   'KeyK': 'audio/K.wav',
+//   'KeyL': 'audio/L.wav',
+// };
 
 let lastKey = null;
 
+const createAudio = (src) => {
+  const audio = new Audio(src);
+  audio.play();
+};
+
+const playSound = (key) => {
+  const audioSrc = key.querySelector('audio').src;
+  createAudio(audioSrc);
+  key.classList.add('play');
+  lastKey = key;
+};
+
+const removePlayClass = () => {
+  if (lastKey !== null) {
+    lastKey.classList.remove('play');
+    lastKey = null;
+  }
+};
+
 document.querySelectorAll('.key').forEach(key => {
   key.addEventListener('mousedown', function(event) {
-    if (lastKey !== null) {
-      lastKey.classList.remove('play');
-    }
-    key.classList.add('play');
-    let audioSrc = key.querySelector('audio').src;
-    let audio = new Audio(audioSrc);
-    audio.play();
-    lastKey = key;
+    removePlayClass();
+    playSound(key);
   });
 });
 
 document.addEventListener('keydown', function(event) {
-  let code = event.code;
-  let key = document.getElementById(code.charCodeAt(3));
+  const code = event.code;
+  const key = document.getElementById(code.charCodeAt(3));
   if (key !== null) {
-    if (lastKey !== null && lastKey !== key) {
-      lastKey.classList.remove('play');
-    }
-    key.classList.add('play');
-    let audioSrc = key.querySelector('audio').src;
-    let audio = new Audio(audioSrc);
-    audio.play();
-    lastKey = key;
+    removePlayClass();
+    playSound(key);
   }
 });
 
 document.addEventListener('keyup', function(event) {
-  let code = event.code;
-  let key = document.getElementById(code.charCodeAt(3));
+  const code = event.code;
+  const key = document.getElementById(code.charCodeAt(3));
   if (key !== null && key !== lastKey) {
     key.classList.remove('play');
-    lastKey = null;
   }
 });
