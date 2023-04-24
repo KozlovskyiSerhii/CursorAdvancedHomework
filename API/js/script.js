927a873d1ffc6177ffd5d3d24434e78dfbaa2ffb
Serhii <kozlovskijserhij@gmail.com>
2023-04-24 22:58:39 +0300

    add person? films planets

--- API/js/script.js
+++ API/js/script.js
@@ -1,23 +1,100 @@
-const buttons = document.querySelectorAll("button");
+const films = document.querySelectorAll(".films");
+const planets = document.querySelectorAll(".planets");
+const persons = document.querySelectorAll(".persons");
+const cards = document.querySelector('.cards');
+let activeElement = null;
 
-buttons.forEach((el, index) => {
-  el.addEventListener("click", () => {
-     getCharacterInfo1(index + 1);
-     getCharacterInfo(index + 1)
-     console.log(index+1);
+function clearCards() {
+  cards.innerHTML = '';
+}
+
+persons.forEach((el, index) => {
+  el.addEventListener("click", async () => {
+    if (activeElement === el) {
+      return; 
+    }
+    clearCards();
+    activeElement = el;
+     let clicked = false;
+     index = 0;
+    for (let j = 0; j <= 5; j++) {
+      await getCharacterInfo(index + 1);
+      index++;
+    }   
   });
 });
 
-function getCharacterInfo(id) {
-  fetch(`https://swapi.dev/api/people/${id}`)
-    .then((res) => res.json())
-    .then((res) => {
-      console.log({ res });
-    });
-}
-
-async function getCharacterInfo1(id) {
+async function getCharacterInfo(id) {
   const request = await fetch(`https://swapi.dev/api/people/${id}`);
   const response = await request.json();
-  console.log({ response });
+  console.log(response);
+  $(".cards").append(`
+    <div class="person-card">
+      <img src="./img/${id}.webp" alt="">
+      <p class="info">Name: ${response.name}</p>
+      <p class="info">born: ${response.birth_year}</p>
+      <p class="info">sex: ${response.gender}</p>
+    </div>`);
+  console.log(id);
+}
+
+films.forEach((el, index) => {
+  el.addEventListener("click", async () => {
+    if (activeElement === el) {
+      return;
+    }
+    clearCards();
+    activeElement = el;
+     let clicked = false;
+     index = 0;
+    for (let j = 0; j <= 5; j++) {
+      await getFilmsInfo(index + 1);
+      index++;
+    }   
+  });
+});
+
+async function getFilmsInfo(id) {
+  const request = await fetch(`https://swapi.dev/api/films/${id}`);
+  const response = await request.json();
+  console.log(response);
+  $(".cards").append(`
+    <div class="person-card">
+      <img src="./img/${id}.jpg" alt="">
+      <p class="info">${response.title}</p>
+      <p class="info">Episode № ${response.episode_id}</p>
+      <p class="info">Release date ${response.release_date}</p>
+    </div>`);
+  console.log(id);
+}
+
+
+planets.forEach((el, index) => {
+  el.addEventListener("click", async () => {
+    if (activeElement === el) {
+      return;
+    }
+    clearCards();
+    activeElement = el;
+     let clicked = false;
+     index = 0;
+    for (let j = 0; j <= 5; j++) {
+      await getPlanetsInfo(index + 1);
+      index++;
+    }   
+  });
+});
+
+async function getPlanetsInfo(id) {
+  const request = await fetch(`https://swapi.dev/api/planets/${id}`);
+  const response = await request.json();
+  console.log(response);
+  $(".cards").append(`
+    <div class="person-card">
+      <img src="./img/${id}.jpg" alt="">
+      <p class="info">${response.name}</p>
+      <p class="info">Rotation period: ${response.rotation_period}</p>
+      <p class="info">${response.climate}</p>
+    </div>`);
+  console.log(id);
 }
